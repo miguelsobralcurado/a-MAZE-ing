e72c9f62f104ab010fa27f8344115190444711e4
miguelsobralcurado <miguel.curado@gmail.com>
2026-03-19 16:09:32 +0000

    On branch more_algo_tests  Changes to be committed: 	modified:   algo_tests/test.py

--- algo_tests/test.py
+++ algo_tests/test.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Generator, Tuple
+from typing import List, Optional, Generator, Tuple, Set
 import random
 
 
@@ -7,46 +7,54 @@ class MazeGrid:
         self.cells: List[List[int]] = list()
         self.width = width
         self.height = height
-        self.pat_coords: List[tuple[int, int]] = []
+        self.pat_coords: Set[tuple[int, int]] = set()
 
     def initialize_all_closed(self) -> None:
         self.cells = [[15 for _ in range(self.width)]
                       for _ in range(self.height)]
-        
-    def add_pattern(self, pattern: List[str]) -> list:
+
+    def add_pattern(self, pattern: List[str]) -> set:
         y_offset = self.height // 2 - len(pattern) // 2
         x_offset = self.width // 2 - len(pattern[0]) // 2
-        self.pat_coords: List[tuple[int, int]] = [
+        self.pat_coords: Set[tuple[int, int]] = {
             (r + y_offset, c + x_offset)
             for r, row in enumerate(pattern)
             for c, col in enumerate(row)
-            if col == "#"]
+            if col == "#"
+        }
         return self.pat_coords
 
+
 class MazeGenerator:
-    def __init__(self, width: int, height: int, seed: Optional[int],
-                 perfect: bool, visual: bool,
-                 algorithm: str = "prims"):
+    def __init__(self, width: int, height: int, entry, exit, seed: int = 0,
+                 perfect: bool = True, algorithm: str = "prims"):
         self.width = width
         self.height = height
+        self.seed = seed
+        self.entry = entry
+        self.exit = exit
         self.perfect = perfect
+        self.maze_grid: MazeGrid = self.initialize()
         self.choose_algorithm(algorithm)
-        self.random = random.Random.seed(seed)
-        self.visual = visual
-        self.maze_grid: MazeGrid
+        self.generator_method = self.choose_generator_method()
 
     # Create grid
     def choose_algorithm(self, algorithm: str) -> None:
         self.algorithm = algorithm.lower().strip()
 
-    def initialize(self, entry: tuple[int, int],
-                 exit: tuple[int, int]) -> MazeGrid:
-        self.validate_params(entry, exit)
+    def initialize(self) -> MazeGrid:
         self.maze_grid = MazeGrid(self.width, self.height)
         self.maze_grid.initialize_all_closed()
         return self.maze_grid
 
-    def set_42(self) -> List[tuple[int, int]]:
+    def validate_entry_exit(self):
+        if self.entry in self.maze_grid.pat_coords:
+            raise ValueError("Entry coordinates on top of 42 logo")
+        if self.exit in self.maze_grid.pat_coords:
+            raise ValueError("Exit coordinates on top of 42 logo")
+        return True
+
+    def set_42(self) -> Set[tuple[int, int]]:
         pat_small = [
             "#.#.###",
             "#.#...#",
@@ -88,169 +96,163 @@ class MazeGenerator:
         self.maze_grid.add_pattern(pattern)
         return self.maze_grid.pat_coords
 
-    def generate(self) -> Generator[List[List[int]], None, None]:
+    def choose_generator_method(self):
         if self.algorithm == "prims":
             return self.prims()
         elif self.algorithm == "dfs":
             raise NotImplementedError("DFS not implemented yet")
 
     def prims(self) -> Generator[List[List[int]], None, None]:
-        generator = PrimsAlgorithm(self.random, self.perfect)
+        generator = PrimsAlgorithm(self.perfect, self.seed)
         return generator.generate_mst(self.maze_grid)
 
 
 class PrimsAlgorithm:
-    def __init__(self):
-         # Bitmask values for walls
+    def __init__(self, perfect, seed: int = 0):
+        # Bitmask values for walls
         self.NORTH = 1
         self.EAST = 2
         self.SOUTH = 4
         self.WEST = 8
-
-    @staticmethod
-    def ignore_logo(visited, maze_grid) -> List[List[bool]]:
-        for node in maze_grid.pat_coords:
-            y, x = node
-            visited[y][x] = True
-        return visited
+        self.perfect = perfect
+        self.seed = seed
+        self.random = random.Random(seed)
 
     def generate_mst(self, maze_grid: MazeGrid) -> Generator[List[List[int]], None, None]:
-        visited: List[List[bool]] = [[False for _ in range(maze_grid.width)]
-                                     for _ in range(maze_grid.height)]
-        visited_count = 0
+        # Minimum Spanning Tree is a list of (y, x, wall_vallue) tuples
+        mst = maze_grid.cells
+        total_nodes = (maze_grid.width ** 2)
+        unvisited = [n for n in range(total_nodes)]
+        current_node = unvisited[int(self.random.random())]
+        visited = [current_node]
+        unvisited.remove(current_node)
 
-        if maze_grid.pat_coords:
-            visited = self.ignore_logo(visited, maze_grid)
-            visited_count += len(maze_grid.pat_coords)
-
-        start = (0, 0)
-        visited[start[0]][start[1]] = True
-        visited_count += 1
-        total = maze_grid.width * maze_grid.height
-        while visited_count < total:
-            edges_pool = self.get_available_edges(visited)
+        while len(unvisited) > len(maze_grid.pat_coords):
+            edges_pool = self.get_available_edges(visited, maze_grid)
             edge = self.random.choice(edges_pool)
-            node, next_node = edge
-            maze_grid.cells = self.open_walls(maze_grid.cells, node, next_node)
-            y, x = next_node
-            visited[y][x] = True
-            visited_count += 1
-            yield maze_grid.cells
-        return maze_grid.cells
+            current_node, next_node = edge
+            mst = self.open_walls(mst, current_node, next_node, maze_grid)
+            visited.append(next_node)
+            unvisited.remove(next_node)
+            yield mst
+        if self.perfect:
+            return mst
 
-    def get_available_edges(self, visited) -> List[Tuple[tuple, tuple]]:
+    def get_available_edges(self, visited, maze_grid) -> List[Tuple[tuple, tuple]]:
         edges_pool = []
 
-        for row in enumerate(visited):
-        # get y = row in [index, list] pairs
-            for col in enumerate(row[1]):
-            #get x = col in [index, bool] pairs
-                if col[1]:
-                    node = (row[0], col[0])
+        for node in visited:
 
-                    if row[0] > 0 and not visited[row[0] - 1][col[0]]:
-                        # all rows except top one have top neighbours
-                        top_node = (row[0] - 1, col[0])
-                        edges_pool.append((node, top_node))
+            row = node // maze_grid.width
+            col = node % maze_grid.width
 
-                    if col[0] > 0 and not visited[row[0]][col[0] - 1]:
-                        # all columns except first have left neighbours
-                        left_node = (row[0], col[0] - 1)
-                        edges_pool.append((node, left_node))
+            if (row, col) not in maze_grid.pat_coords:
+                if row > 0:
+                    # all rows except top one have top neighbours
+                    top_node = node - maze_grid.width
+                    if top_node not in visited:
+                        if (top_node // maze_grid.width, top_node % maze_grid.width) not in maze_grid.pat_coords:
+                            edges_pool.append((node, top_node))
 
-                    if row[0] < len(visited) - 1 and not visited[row[0] + 1][col[0]]:
-                        # all rows except last one have bot neighbours
-                        bot_node = (row[0] + 1, col[0])
-                        edges_pool.append((node, bot_node))
+                if col > 0:
+                    # all columns except first have left neighbours
+                    left_node = node - 1
+                    if left_node not in visited:
+                        if (left_node // maze_grid.width, left_node % maze_grid.width) not in maze_grid.pat_coords:
+                            edges_pool.append((node, left_node))
 
-                    if col[0] < len(visited[0]) - 1 and not visited[row[0]][col[0] + 1]:
-                        # all columns except last have right neighbours
-                        right_node = (row[0], col[0] + 1)
-                        edges_pool.append((node, right_node))
+                if row < maze_grid.width - 1:
+                    # all rows except last one have bot neighbours
+                    bot_node = node + maze_grid.width
+                    if bot_node not in visited:
+                        if (bot_node // maze_grid.width, bot_node % maze_grid.width) not in maze_grid.pat_coords:
+                            edges_pool.append((node, bot_node))
+
+                if col < maze_grid.width - 1:
+                    # all columns except last have right neighbours
+                    right_node = node + 1
+                    if right_node not in visited:
+                        if (right_node // maze_grid.width, right_node % maze_grid.width) not in maze_grid.pat_coords:
+                            edges_pool.append((node, right_node))
 
         return edges_pool
 
-    def open_walls(self, cells: List[List[int]], node: tuple, next_node: tuple):
-        y, x = node
-        n_y, n_x = next_node
-        if y - 1 == n_y:
-            cells[y][x] = cells[y][x] ^ self.NORTH
-            cells[n_y][n_x] = cells[n_y][n_x] ^ self.SOUTH
-        if x + 1 == n_x:
-            cells[y][x] = cells[y][x] ^ self.EAST
-            cells[n_y][n_x] = cells[n_y][n_x] ^ self.WEST
-        if y + 1 == n_y:
-            cells[y][x] = cells[y][x] ^ self.SOUTH
-            cells[n_y][n_x] = cells[n_y][n_x] ^ self.NORTH
-        if x - 1 == n_x:
-            cells[y][x] = cells[y][x] ^ self.WEST
-            cells[n_y][n_x] = cells[n_y][n_x] ^ self.EAST
-        return cells
+    def open_walls(self, mst: List[List[int]], node: int, next_node: int, maze_grid):
+        y = node // maze_grid.width
+        x = node % maze_grid.width
+        n_y = next_node // maze_grid.width
+        n_x = next_node % maze_grid.width
+        if node - maze_grid.width == next_node:
+            mst[y][x] ^= self.NORTH
+            mst[n_y][n_x] ^= self.SOUTH
+        if node - 1 == next_node:
+            mst[y][x] ^= self.EAST
+            mst[n_y][n_x] ^= self.WEST
+        if node + maze_grid.width == next_node:
+            mst[y][x] ^= self.SOUTH
+            mst[n_y][n_x] ^= self.NORTH
+        if node + 1 == next_node:
+            mst[y][x] ^= self.WEST
+            mst[n_y][n_x] ^= self.EAST
+        return mst
 
 
+def main():
+    entry = (0, 0)
+    exit = (19, 14)
+    maze_gen = MazeGenerator(21, 21, entry, exit, 0)
 
-maze: List[List[int]] = {
-    [15, 15, 15, 15, 15],
-    [15, 15, 15, 15, 15],
-    [15, 14, 15, 15, 15],
-    [15, 14, 15, 15, 15],
-    [15, 15, 15, 15, 15],
-}
+#    ascii_rend = AsciiRenderer(bla bla)
 
-algorithm = gen.algorithm()
+    maze_grid = maze_gen.initialize()
+    test_grid = maze_grid.cells
 
-for _ in algorithm:
+#    ascii_rend.draw_maze(maze_grid.cells)
+    # List 2D com tudo a 15
+
+    logo = maze_gen.set_42()
+    # Lista tuples y, x para aplicar directamente na grid?
+
+    try:
+        maze_gen.validate_entry_exit()
+    except ValueError as e:
+        print(e)
+    else:
+        print("Entry and exit values validated")
+
+#    ascii_rend.paint(logo)
+
+    for cell in logo:
+        y, x = cell
+#        ascii_rend.maze_colors[y][x] = 42
+        test_grid[y][x] = 42
+    y, x = entry
+#    ascii_rend.maze_colors[y][x] = 2
+    test_grid[y][x] = 20
+    y, x = exit
+#    ascii_rend.maze_colors[y][x] = 3
+    test_grid[y][x] = 50
+    # Adicionar entry e exit ao maze_grid
+
+    for row in test_grid:
+        print(row)
+    print()
+
+    maze_generator = maze_gen.generator_method
+    # Generator passo a passo do algoritmo
+    # Um tuple por passo (y, x, valor paredes) para aplicar directamente na grid?
+    # Lista completa no fim
+
+    next_grid = next(maze_generator)
+    for row in next_grid:
+        print(row)
+#    for _ in maze_generator:
+    #     next_grid = next(maze_generator)
+    #     for row in next_grid:
+    #         print(row)
+    #     print()
+    # print()
 
 
-
-
-
-
-
-
-
-
-
-"""
-A-Maze-ing — reusable maze generator module
-
-Standalone, reusable module:
-- Maze data model (walls bitmask)
-- Generation algorithms + constraints
-- Solver (shortest path)
-
-Rules:
-- No printing
-- No file I/O
-- No user interaction
-
-Short documentation
-------------------------------
-1) Instantiate and use the generator (basic example):
-    from mazegen import MazeGenerator
-    gen = MazeGenerator(width=31, height=21, seed=42,
-                              perfect=True, algorithm="dfs")
-    entry = (1, 1)
-    exit_ = (29, 19)
-    maze = gen.generate(entry=entry, exit=exit_)
-    path = gen.solve(maze=maze, entry=entry, exit=exit_)
-
-2) Pass custom parameters (size, seed, etc.):
-    - width, height: maze dimensions (int)
-    - seed: int or None (None => random)
-    - perfect: bool (True => perfect maze; False => may create loops)
-    - algorithm: "dfs" or "prim"
-    - density: float (used when perfect=False)
-
-3) Access the generated structure and a solution:
-    - maze.width, maze.height
-    - maze.cells[y][x]: int bitmask in 0..15
-      Bits: N=1, E=2, S=4, W=8 (bit set => wall is CLOSED)
-    - "42" stamp: maze.omitted_42 and maze.stamp42 (if present)
-    - path: list of moves like ["N", "E", ...] from solve(...)
-
-Packaging note
---------------
-This file must be packagable as mazegen-* (.whl or .tar.gz) and installable
-via pip.
-"""
+if __name__ == "__main__":
+    main()
